597084f725f4b5bb9494ea987f839d38c256a74a
tanv000 <tanv79740@gmail.com>
2025-09-27 18:48:40 +0530

    day2 adv problems solved

--- dsa/day3_adv4.java
+++ dsa/day3_adv4.java
@@ -5,9 +5,42 @@ trap after raining.
  Output:  6
  */
 
+import java.util.Scanner;
+
 public class day3_adv4 {
-    public static void main(String args[])
-    {
-        
+
+    public static int computeWater(int[] arr) {
+        int n = arr.length;
+        int lMax = 0, rMax = 0, total = 0, l = 0, r = n - 1;
+
+        while (l < r) {
+            if (arr[l] <= arr[r]) {
+                if (lMax > arr[l])
+                    total += lMax - arr[l];
+                else
+                    lMax = arr[l];
+
+                l++;
+            } else {
+                if (rMax > arr[r])
+                    total += rMax - arr[r];
+                else
+                    rMax = arr[r];
+
+                r--;
+            }
+        }
+        return total;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(computeWater(arr));
     }
 }
